b5831ef8cfb366b8c41ac65dacab6c27a0f29bdd
mohamim360 <mohamim360@gmail.com>
2023-11-23 23:44:45 +0600

    eslint,prettier

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+export default app;
